5b1251e45e5ddb46d9786cfbd542a778df8c9248
Julian Rice <jrice15@calpoly.edu>
2022-07-14 15:33:41 -0700

    chm patching works

--- pycrown/chm_to_patches.py
+++ pycrown/chm_to_patches.py
@@ -3,8 +3,9 @@ import glob
 import os
 import subprocess
 import tempfile
+from pathlib import PurePath
 
-dirn = os.path.dirn
+dirn = os.path.dirname
 ROOT = PurePath(dirn(dirn(os.path.abspath(__file__))))
 
 parser = argparse.ArgumentParser()
@@ -16,19 +17,24 @@ ARGS = parser.parse_args()
 outdir = os.path.join(ARGS.out_dir)
 os.makedirs(outdir, exist_ok=True)
 
+bounds_file = "_TEMP_CHM_TILE_BOUNDS_TEMP.gpkg"
+if os.path.exists(bounds_file):
+    os.remove(bounds_file)
 
-for naip_path in glob.glob((ROOT / f"data/NAIP_patches/{region}/*.tif").as_posix()):
+for naip_path in glob.glob((ROOT / f"data/NAIP_patches/{ARGS.region}/*.tif").as_posix()):
     patch_num = PurePath(naip_path).stem.split("_")[-1]
 
-    bounds_file = tempfile.NamedTemporaryFile(suffix=".gpkg")
     subprocess.run(
-        f"gdaltindex {bounds_file.name} {naip_path}"
+        f"gdaltindex {bounds_file} {naip_path}",
+        shell=True,
     )
 
-    for chm_path in glob.glob(os.path.join(ARGS.chm_path, "*.tif")):
+    for chm_path in glob.glob(os.path.join(ARGS.chm_dir, "*.tif")):
         chm_name = PurePath(chm_path).stem
         out_file = os.path.join(ARGS.out_dir, chm_name + "_" + patch_num + ".tif")
         subprocess.run(
-            f"gdalwarp -cutline {bounds_file.name} -crop_to_cutlin {chm_path} {out_file}"
+            f"gdalwarp -cutline {bounds_file} -crop_to_cutline {chm_path} {out_file}",
+            shell=True,
         )
 
+    os.remove(bounds_file)
